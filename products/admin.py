6d1cf14a8e822cf3b03c43ff8bd43f4246eff124
Ajeet Bisht <40465861+Ajeetbisht@users.noreply.github.com>
2021-07-05 22:07:51 +0530

    Add files via upload

---
+++ products/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ColorVariant)
+admin.site.register(Sizevariant)
+admin.site.register(QuantityVariant)
